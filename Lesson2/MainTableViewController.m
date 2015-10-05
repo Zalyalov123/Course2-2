6c8f698984d96a3c38957c1b566478614acd5dc6
Ильда  Залялов <IldarZalyalov@MacBook-Pro-Ilda.local>
2015-10-05 19:42:30 +0300

    first commit

--- Lesson2/MainTableViewController.m
+++ Lesson2/MainTableViewController.m
@@ -8,11 +8,41 @@
 
 #import "MainTableViewController.h"
 #import "LoadingTableViewController.h"
+@interface MainTableViewController ()
+{
+    NSMutableArray *arrItem;
+}
+@end
 
 @implementation MainTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    arrItem = [NSMutableArray arrayWithObjects:@"Hello",@"Hello2",@"Hello3",@"Hello4", nil];
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return arrItem.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static NSString *cellIdentifier = @"cell";
+    
+    UITableViewCell *cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    cell.textLabel.text = arrItem[indexPath.row];
+    return cell;
+}
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return UITableViewCellEditingStyleDelete;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    [arrItem removeObjectAtIndex:indexPath.row];
 }
 
 @end
